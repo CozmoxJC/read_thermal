05f626330aaba5e4ef974680dbe467b9c9d45a60
CozmoxJC <62492514+CozmoxJC@users.noreply.github.com>
2022-07-05 17:21:50 +0800

    Add files via upload

---
+++ thanos_extract_thermal.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+python flir_image_extractor.py -csv 'temp_img1' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3150.jpg'
+
+python flir_image_extractor.py -csv 'temp_img2' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3151.jpg'
+
+python flir_image_extractor.py -csv 'temp_img3' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3152.jpg'
+
+python flir_image_extractor.py -csv 'temp_img4' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3153.jpg'
+
+python flir_image_extractor.py -csv 'temp_img5' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3154.jpg'
+
+python flir_image_extractor.py -csv 'temp_img6' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3155.jpg'
+
+python flir_image_extractor.py -csv 'temp_img7' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3156.jpg'
+
+python flir_image_extractor.py -csv 'temp_img8' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3157.jpg'
+
+python flir_image_extractor.py -csv 'temp_img9' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3158.jpg'
+
+python flir_image_extractor.py -csv 'temp_img10' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3159.jpg'
+
+python flir_image_extractor.py -csv 'temp_img11' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3160.jpg'
+
+python flir_image_extractor.py -csv 'temp_img12' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3161.jpg'
+
+python flir_image_extractor.py -csv 'temp_img13' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3162.jpg'
+
+python flir_image_extractor.py -csv 'temp_img14' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3163.jpg'
+
+python flir_image_extractor.py -csv 'temp_img15' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3164.jpg'
+
+python flir_image_extractor.py -csv 'temp_img16' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3165.jpg'
+
+python flir_image_extractor.py -csv 'temp_img17' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3166.jpg'
+
+python flir_image_extractor.py -csv 'temp_img18' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3167.jpg'
+
+python flir_image_extractor.py -csv 'temp_img19' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3168.jpg'
+
+python flir_image_extractor.py -csv 'temp_img20' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3169.jpg'
+
+python flir_image_extractor.py -csv 'temp_img21' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3170.jpg'
+
+python flir_image_extractor.py -csv 'temp_img22' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3171.jpg'
+
+python flir_image_extractor.py -csv 'temp_img23' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3172.jpg'
+
+python flir_image_extractor.py -csv 'temp_img24' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3173.jpg'
+
+python flir_image_extractor.py -csv 'temp_img25' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3174.jpg'
+
+python flir_image_extractor.py -csv 'temp_img26' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3175.jpg'
+
+python flir_image_extractor.py -csv 'temp_img27' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3176.jpg'
+
+python flir_image_extractor.py -csv 'temp_img28' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3177.jpg'
+
+python flir_image_extractor.py -csv 'temp_img29' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3178.jpg'
+
+python flir_image_extractor.py -csv 'temp_img30' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3179.jpg'
+
+python flir_image_extractor.py -csv 'temp_img31' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3180.jpg'
+
+python flir_image_extractor.py -csv 'temp_img32' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3181.jpg'
+
+python flir_image_extractor.py -csv 'temp_img33' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3182.jpg'
+
+python flir_image_extractor.py -csv 'temp_img34' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3183.jpg'
+
+python flir_image_extractor.py -csv 'temp_img35' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3184.jpg'
+
+python flir_image_extractor.py -csv 'temp_img36' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3185.jpg'
+
+python flir_image_extractor.py -csv 'temp_img37' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3186.jpg'
+
+python flir_image_extractor.py -csv 'temp_img38' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3187.jpg'
+
+python flir_image_extractor.py -csv 'temp_img39' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3188.jpg'
+
+python flir_image_extractor.py -csv 'temp_img40' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3189.jpg'
+
+python flir_image_extractor.py -csv 'temp_img41' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3190.jpg'
+
+python flir_image_extractor.py -csv 'temp_img42' -i '/content/drive/MyDrive/0701/thanos_circle/FLIR3191.jpg'
