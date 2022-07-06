1dcb63b1e53b1c02032986fe27150a3cb3552dc5
CozmoxJC <62492514+CozmoxJC@users.noreply.github.com>
2022-07-07 02:51:15 +0800

    Add files via upload

---
+++ windblade_h2plate_extract_thermal.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3378.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3379.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3380.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3381.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3382.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3383.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3384.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3385.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3386.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3387.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3388.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3389.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3390.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3391.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3392.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3393.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3394.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3395.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3396.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3397.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3398.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3399.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3400.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3401.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3402.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3403.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3404.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3405.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3406.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3407.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3408.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3409.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3410.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3411.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3412.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3413.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3414.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h2/FLIR3415.jpg'
---
+++ windblade_h3plate_extract_thermal.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3329.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3330.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3331.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3332.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3333.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3334.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3335.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3336.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3337.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3338.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3339.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3340.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3342.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3343.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3344.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3345.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3346.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3347.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3348.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3349.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3350.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3351.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3352.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3353.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3354.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3355.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3356.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3357.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3358.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3359.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3360.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3361.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3362.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3363.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3364.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3365.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3366.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3367.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3368.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3369.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3370.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3371.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3372.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3373.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3374.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3375.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h3/FLIR3376.jpg'
