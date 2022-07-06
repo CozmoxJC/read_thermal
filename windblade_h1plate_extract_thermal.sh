e7a6e6bb243371fd7ac223886686179be59f2026
CozmoxJC <62492514+CozmoxJC@users.noreply.github.com>
2022-07-07 02:23:11 +0800

    Add files via upload

---
+++ windblade_h1plate_extract_thermal.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3417.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3418.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3419.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3420.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3421.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3422.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3423.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3424.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3425.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3426.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3427.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3428.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3429.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3430.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3431.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3432.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3433.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3434.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3435.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3436.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3437.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3438.jpg'
+
+python flir_image_extractor.py -i '/content/drive/MyDrive/0701/windblade_h1/FLIR3439.jpg'
